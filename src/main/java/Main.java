9e3a89f524d6eba5874bcfd06ae041013bc7a5d8
Валентин <valmak04@gmail.com>
2021-10-01 17:29:10 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
+        Scanner sc = new Scanner(System.in);
+        int end_digit = sc.nextInt();
+        for (int count_on_line = 1; count_on_line < end_digit+1; count_on_line += 1) {
+            for (int count_on_string = 0; count_on_string < end_digit - count_on_line; count_on_string += 1) {
+                System.out.print(" ");
+            }
+            for (int stars = 0; stars < count_on_line*2-1; stars += 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+        }
     }
-}
+
